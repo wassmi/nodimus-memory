a2a5887952ff07ba4ccb26798e43669ac6168340
macbook air <macbookair@macbooks-MacBook-Air.local>
2025-08-16 19:49:59 -0400

    fix: Correct seccomp library usage

--- internal/seccomp/seccomp.go
+++ internal/seccomp/seccomp.go
@@ -10,14 +10,18 @@ import (
 
 // LoadProfile loads the seccomp profile.
 func LoadProfile() error {
-	filter, err := libseccomp.NewFilter(libseccomp.ActAllow)
+	filter, err := seccomp.NewFilter(seccomp.ActAllow)
 	if err != nil {
 		return fmt.Errorf("failed to create seccomp filter: %w", err)
 	}
 
 	// Add rules to the filter here.
 	// For example, to deny the "execve" syscall:
-	// if err := filter.AddRule(libseccomp.ScmpSyscall("execve"), libseccomp.ActErrno.SetReturnCode(int16(syscall.EPERM))); err != nil {
+	// execveSyscall, err := seccomp.GetSyscallFromName("execve")
+	// if err != nil {
+	// 	return fmt.Errorf("failed to get syscall number: %w", err)
+	// }
+	// if err := filter.AddRule(execveSyscall, seccomp.ActErrno.SetReturnCode(int16(syscall.EPERM))); err != nil {
 	// 	return fmt.Errorf("failed to add seccomp rule: %w", err)
 	// }
 
